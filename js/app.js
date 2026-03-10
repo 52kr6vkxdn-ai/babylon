a236ac7ddff1ebaebfad281638c90366ff246bc4
52kr6vkxdn-ai <52kr6vkxdn@privaterelay.appleid.com>
2026-03-09 20:37:51 -0400

    Delete js/app.js

--- js/app.js
+++
@@ -1,6 +0,0 @@
-import { greet } from "./utils.js";
-const el = document.getElementById("output");
-el.innerText = greet();
-
-// Intentional Error to demo error mapping:
-// console.log(nonExistentVariable);
